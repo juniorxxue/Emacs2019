b47da383089ed18be64a53a09ada696541f04af2
Shawn Xu <xuuexu@gmail.com>
2018-12-30 23:53:01 +0800

    melpa -> emacs-china mirror

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -14,15 +14,15 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://elpa.emacs-china.org/melpa/")) t)
   ;; Official MELPA Mirror, in case necessary.
   ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
   (if (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
-      (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
+      (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.emacs-china.org/elpa")))
     (unless no-ssl
       ;; Force SSL for GNU ELPA
-      (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))))
+      (setcdr (assoc "gnu" package-archives) "http://elpa.emacs-china.org/gnu/"))))
 
 
 ;;; On-demand installation of packages
