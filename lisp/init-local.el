ff757f2ea8441faa52b92a191ae423d7d067df2e
Shawn Xu <xuuexu@gmail.com>
2019-01-03 15:41:48 +0800

    auto save

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -9,9 +9,24 @@
 (require 'doom-modeline)
 (doom-modeline-init)
 
+;; for macbook-pro
+(add-to-list 'exec-path "/usr/local/bin/")
+
 ;; auto save
 (require-package 'super-save)
 (require 'super-save)
 (super-save-mode +1)
+
+;; company
+(setq company-idle-delay 0
+      company-minimum-prefix-length 1)
+
+;; highlight current line
+(global-hl-line-mode 1)
+
+;; display line number
+(global-display-line-numbers-mode)
+
+(load "/Users/xuexu/Desktop/Scheme/doom-xcode-dark-theme.el")
 
 (provide 'init-local)
