42f73f6164527d0123fa56892c4d379a32f91a10
Shawn Xu <xuuexu@gmail.com>
2019-01-23 14:18:04 +0800

    Company icons && lsp-python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,6 +1,7 @@
 ;;; Be sure to install
 ;; setuptools
 ;; autopep8
+;; python-language-server
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
                 ("SConscript\\'" . python-mode))
@@ -12,14 +13,26 @@
 (setenv "PYTHONPATH" "/Users/xuexu/.pyenv/shims/python")
 (require-package 'pip-requirements)
 
-(when (maybe-require-package 'anaconda-mode)
-  (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (after-load 'python
-        (push 'company-anaconda company-backends)))))
+;; (when (maybe-require-package 'anaconda-mode)
+;;   (after-load 'python
+;;     (add-hook 'python-mode-hook 'anaconda-mode)
+;;     (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+;;   (when (maybe-require-package 'company-anaconda)
+;;     (after-load 'company
+;;       (after-load 'python
+;;         (push 'company-anaconda company-backends)))))
+(require-package 'lsp-mode)
+(require 'lsp-mode)
+(require-package 'lsp-ui)
+(require-package 'company-lsp)
+
+(require 'lsp-ui)
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+(require 'company-lsp)
+;; (require-package 'lsp-python)
+(require 'lsp-clients)
+(add-hook 'python-mode-hook 'lsp)
+
 
 
 (require-package 'py-autopep8)
